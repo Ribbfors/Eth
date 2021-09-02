fdd477a9606dc939f5e0a6555538c6b041a8bb6b
Ribz <andreas.ribbfors@icloud.com>
2021-09-02 15:25:11 +0200

    test3

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,6 @@ module.exports = function (app) {
     createProxyMiddleware("/v1", {
       target: "https://pro-api.coinmarketcap.com",
       changeOrigin: true,
-      secure: false,
     })
   );
 };
